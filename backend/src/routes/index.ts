f4ddc02022e0b7859f45ae461a79507724d036f4
Gabriel <gabrielmvelezmoro592@gmail.com>
2021-06-23 13:05:24 -0300

    remove useless folders and entyties

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 import userRouter from './user.routes';
 import sessionRouter from './session.routes';
-import moduleRouter from './module.routes';
 import menuOptionRouter from './menuOption.routes';
 import profileRouter from './profile.routes';
 
@@ -10,7 +9,6 @@ const routes = Router();
 
 routes.use('/user', userRouter);
 routes.use('/session', sessionRouter);
-routes.use('/module', moduleRouter);
 routes.use('/menu-option', menuOptionRouter);
 routes.use('/profiles', profileRouter);
 
